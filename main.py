940da82f78564a3dbd75608814117518878ed14a
Cyberbolt <735245473@qq.com>
2022-10-13 16:44:39 +0800

    Project directory initialization.

--- main.py
+++ main.py
@@ -1,18 +1,12 @@
 import pprint
-import subprocess
+
+from cfw import get_ip
 
 
 def main():
-    r = subprocess.run(
-        "ss -Hntu | awk '{print $6}' | sort | uniq -c | sort -nr", 
-        shell=True,
-        capture_output=True
-    )
-    text = r.stdout.decode()
-    
-    new = [line.strip().rsplit(' ', 1) for line in text.split('\n')[:-1]]
-    pprint.pprint(new)
+    form = get_ip()
+    pprint.pprint(form)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
