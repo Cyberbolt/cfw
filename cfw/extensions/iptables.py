56fde21e28b6577ac2421df53aed13cdc8a2d874
Cyberbolt <735245473@qq.com>
2022-10-20 09:18:30 +0000

    The master branch is restored.

--- cfw/extensions/iptables.py
+++ cfw/extensions/iptables.py
@@ -2,58 +2,10 @@
     Call the Linux iptables API.
 """
 
-# import ipaddress
-# import subprocess
-
-# from ..CFWError import WhitelistCFWError
-
-
-# def shell(cmd: str) -> str:
-#     """
-#         Execute a shell statement and return the output.
-#     """
-#     r = subprocess.run(
-#         cmd, 
-#         shell=True,
-#         capture_output=True
-#     )
-#     return r.stdout.decode()
-
-
-# class Whitelist(list):
-    
-#     def __init__(self, path: str):
-#         """
-#             path: Path to the whitelist file
-#         """
-#         with open(path, 'r') as f:
-#             text = f.read()
-#         for ip in text.splitlines():
-#             try:
-#                 ip = str(ipaddress.IPv4Address(ip))
-#             except ipaddress.AddressValueError:
-#                 try:
-#                     ip = str(ipaddress.IPv4Network(ip))
-#                 except ipaddress.NetmaskValueError:
-#                     raise WhitelistCFWError("The whitelist ip format is incorrect.")
-#             self.append(ip)
-
-# whitelist = Whitelist("whitelist.txt")
-
-
-# def block_ip(ip: str):
-#     for ip_w in whitelist:
-#         if ipaddress.IPv4Address(ip) in ipaddress.IPv4Network(ip_w):
-#             print('sadasdasddsa')
-#             return
-#     shell(f"ipset add blacklist {ip}")
-
-
-# shell("ipset create blacklist hash:net")
-# shell("iptables -I INPUT -m set --match-set blacklist src  -p tcp -j DROP")
-
-
+import ipaddress
 import subprocess
+
+from ..CFWError import WhitelistCFWError
 
 
 def shell(cmd: str) -> str:
@@ -63,54 +15,39 @@ def shell(cmd: str) -> str:
     r = subprocess.run(
         cmd, 
         shell=True,
-        # capture_output=True,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-        encoding="utf-8"
+        capture_output=True
     )
-    # return r.stdout.decode()
-    return r
+    return r.stdout.decode()
 
-def cfw_init():
-    """
-    cfw initialization, open port 22 and loopback address by default.
-    """
-    shell("iptables -A INPUT -p tcp --dport  22 -j ACCEPT")
-    shell("iptables -P INPUT DROP")
-    shell("iptables -P FORWARD DROP")
-    shell("iptables -P OUTPUT DROP")
-    shell("iptables -A INPUT -i lo -j ACCEPT")
-    shell("iptables -A OUTPUT -o lo -j ACCEPT")
-    shell("iptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT")
+
+class Whitelist(list):
     
-def close_22():
-    """
-    close port 22
-    """
-    try:
-        shell("iptables -D INPUT -p tcp --dport  22 -j ACCEPT")
-        shell("iptables -A INPUT -p tcp --dport  22 -j DROP")
-    except:
-        pass
+    def __init__(self, path: str):
+        """
+            path: Path to the whitelist file
+        """
+        with open(path, 'r') as f:
+            text = f.read()
+        for ip in text.splitlines():
+            try:
+                ip = str(ipaddress.IPv4Address(ip))
+            except ipaddress.AddressValueError:
+                try:
+                    ip = str(ipaddress.IPv4Network(ip))
+                except ipaddress.NetmaskValueError:
+                    raise WhitelistCFWError("The whitelist ip format is incorrect.")
+            self.append(ip)
 
-def open_22():
-    """
-    open port 22
-    """
-    try:
-        shell("iptables -D INPUT -p tcp --dport  22 -j DROP")
-        shell("iptables -A INPUT -p tcp --dport  22 -j ACCEPT")
-    except:
-        pass
+whitelist = Whitelist("whitelist.txt")
 
-def ddos_protection():
-    """
-    Use cfw for ddos protection.
-    """
-    # DOS
-    shell("iptables -A INPUT -i eth0 -p tcp --syn -m connlimit --connlimit-above 15 -j DROP")
-    shell("iptables -A INPUT -p tcp -m state --state ESTABLISHED,RELATED -j ACCEPT")
-    
-    # DDOS
-    shell("iptables -A INPUT  -p tcp --syn -m limit --limit 12/s --limit-burst 24 -j DROP")  
-    shell("iptables -A FORWARD -p tcp --syn -m limit --limit 1/s -j ACCEPT ")
+
+def block_ip(ip: str):
+    for ip_w in whitelist:
+        if ipaddress.IPv4Address(ip) in ipaddress.IPv4Network(ip_w):
+            print('sadasdasddsa')
+            return
+    shell(f"ipset add blacklist {ip}")
+
+
+shell("ipset create blacklist hash:net")
+shell("iptables -I INPUT -m set --match-set blacklist src  -p tcp -j DROP")
--- cfw/ip_tools.py
+++ cfw/ip_tools.py
@@ -1,77 +1,77 @@
-# """
-#     Get and handle socket connections.
-# """
+"""
+    Get and handle socket connections.
+"""
 
-# import time
-# import yaml
+import time
+import yaml
 
-# import numpy as np
-# import pandas as pd
-# from apscheduler.schedulers.background import BackgroundScheduler
+import numpy as np
+import pandas as pd
+from apscheduler.schedulers.background import BackgroundScheduler
 
-# from .extensions import iptables, shell
-# from .CFWError import ConfigurationCFWError
+from .extensions import iptables, shell
+from .CFWError import ConfigurationCFWError
 
-# with open("config.yaml") as f:
-#     config = yaml.load(f, Loader=yaml.FullLoader)
-
-
-# def get_ip() -> pd.DataFrame:
-#     """
-#         Get the current socket connection and convert it to a DataFrame.
-#     """
-#     text = shell("ss -Hntu | awk '{print $5,$6}' ")
-#     data = []
-#     for line in text.split("\n")[:-1]:
-#         server, client = line.split(" ")
-#         server_ip, server_port = server.split(":")
-#         client_ip, client_port = client.split(":")
-#         data.append([server_ip, server_port, client_ip, client_port])
-#     data_df = pd.DataFrame(
-#         data, 
-#         columns=["server_ip", "server_port", "client_ip", "client_port"]
-#     )
-#     return data_df
+with open("config.yaml") as f:
+    config = yaml.load(f, Loader=yaml.FullLoader)
 
 
-# def load_config():
-#     if not config.get("global"):
-#         raise ConfigurationCFWError(
-#             "'global' was not found in the yaml file."
-#         )
-#     if not config["global"].get("frequency"):
-#         raise ConfigurationCFWError(
-#             "'frequency' is not set in 'global'."
-#         )
-#     if not config["global"].get("max_num"):
-#         raise ConfigurationCFWError(
-#             "'max_num' is not set in 'global'."
-#         )
-#     if not config["global"].get("unblock"):
-#         raise ConfigurationCFWError(
-#             "'unblock' is not set in 'global'."
-#         )
+def get_ip() -> pd.DataFrame:
+    """
+        Get the current socket connection and convert it to a DataFrame.
+    """
+    text = shell("ss -Hntu | awk '{print $5,$6}' ")
+    data = []
+    for line in text.split("\n")[:-1]:
+        server, client = line.split(" ")
+        server_ip, server_port = server.split(":")
+        client_ip, client_port = client.split(":")
+        data.append([server_ip, server_port, client_ip, client_port])
+    data_df = pd.DataFrame(
+        data, 
+        columns=["server_ip", "server_port", "client_ip", "client_port"]
+    )
+    return data_df
 
 
-# def block_ips_run():
-#     data = get_ip()
-#     data = data.groupby(["client_ip"], as_index = False)["client_ip"].agg({
-#         "num": np.size
-#     }).sort_values(by="num", ascending=False, ignore_index=True)
-#     ips = []
-#     for index, row in data.iterrows():
-#         if row["num"] > config["global"]["max_num"]:
-#             ips.append(row["client_ip"])
-#     for ip in ips:
-#         iptables.block_ip(ip)
+def load_config():
+    if not config.get("global"):
+        raise ConfigurationCFWError(
+            "'global' was not found in the yaml file."
+        )
+    if not config["global"].get("frequency"):
+        raise ConfigurationCFWError(
+            "'frequency' is not set in 'global'."
+        )
+    if not config["global"].get("max_num"):
+        raise ConfigurationCFWError(
+            "'max_num' is not set in 'global'."
+        )
+    if not config["global"].get("unblock"):
+        raise ConfigurationCFWError(
+            "'unblock' is not set in 'global'."
+        )
 
 
-# load_config()
+def block_ips_run():
+    data = get_ip()
+    data = data.groupby(["client_ip"], as_index = False)["client_ip"].agg({
+        "num": np.size
+    }).sort_values(by="num", ascending=False, ignore_index=True)
+    ips = []
+    for index, row in data.iterrows():
+        if row["num"] > config["global"]["max_num"]:
+            ips.append(row["client_ip"])
+    for ip in ips:
+        iptables.block_ip(ip)
 
-# sched = BackgroundScheduler(timezone="Asia/Shanghai")
-# sched.add_job(
-#     block_ips_run, 
-#     'interval', 
-#     seconds=config["global"]["frequency"]
-# )
-# sched.start()
+
+load_config()
+
+sched = BackgroundScheduler(timezone="Asia/Shanghai")
+sched.add_job(
+    block_ips_run, 
+    'interval', 
+    seconds=config["global"]["frequency"]
+)
+sched.start()
