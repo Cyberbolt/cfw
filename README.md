9e8838b0e25314b07003b1a5c5b351d3715aa6de
Cyberbolt <735245473@qq.com>
2022-11-11 13:43:52 +0000

    Added uninstall script

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
 # cfw
+
 CFW (Cyber Firewall) is a human friendly Linux firewall. (In Development)
+
+# 中文版
+
+CFW (Cyber Firewall) 是一个人性化的 Linux 防火墙。它旨在协助阻止拒绝服务攻击，同时能控制 Linux 端口开关。
+
+该软件基于 iptables 和 ipset，使用 Python 开发，使用时建议关闭发行版自带的防火墙(如 firewalld、ufw)避免冲突。
+
+通过 CFW，您将能够：
+
+- 通过自定义的规则自动封禁互联网中的恶意 ip，以防止拒绝服务攻击
+
+- 控制开启或关闭 Linux 的 TCP/UDP 端口
+
+- 获得友好的交互式体验
+
+如欲配合 CDN 使用，请将 CDN 的 ip 段设置为 CFW 白名单。
+
+### 实现方法
+
+CFW 通过 `ss -Hntu | awk '{print $5,$6}'` 命令获取当前服务器的所有连接。客户端的请求若超过设定并发数，该 ip 将被 iptables 封禁，并存储在 ipset 数据结构中。
+
+### 安装
+
+请先确保系统拥有以下依赖：
+
+示例 1 (Debian, Ubuntu)
+
+```
+apt install -y curl ipset python3 git net-tools
+```
+
+示例 2 (CentOS)
+
+```
+yum install -y curl ipset python3 git net-tools
+```
+
+安装好系统依赖后，输入以下命令安装 CFW：
+
+```
+curl https://raw.githubusercontent.com/Cyberbolt/cfw/main/install.py | python3
+```
+
+完成安装后，使用 `source ~/.bashrc` 激活 CFW 的环境变量。(新开 shell 将自动激活环境变量)
---
+++ uninstall.py
@@ -0,0 +1,47 @@
+import sys
+import subprocess
+
+
+def cmd(cmd: str):
+    try:
+        subprocess.run(
+            cmd, 
+            shell=True,
+            check=True
+        )
+        return True
+    except subprocess.CalledProcessError as e:
+        sys.exit(1)
+
+
+def shell(cmd: str) -> str:
+    """
+        Execute a shell statement and return the output.
+    """
+    r = subprocess.run(
+        cmd, 
+        shell=True,
+        check=True,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT
+    )
+    return r.stdout.decode()
+
+
+def main():
+    # Remove cfw
+    cmd("rm -rf /etc/cfw/")
+    # Delete environment variable
+    with open("~/.bashrc", "r") as f:
+        text = f.read()
+    lines = text.splitlines()
+    text_new = ""
+    for line in lines:
+        if "alias cfw='/etc/cfw/py39/bin/python /etc/cfw/client.py'" not in line:
+            text_new += line + "\n"
+    with open("~/.bashrc", "w") as f:
+        f.write(text_new)
+
+
+if __name__ == "__main__":
+    main()
